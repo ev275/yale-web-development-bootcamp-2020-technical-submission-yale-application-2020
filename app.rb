cccd7bc2e881651fefd32e283590ab8efcd25d5a
Enrique Valencia <enrique.valencia@yale.edu>
2020-04-21 21:07:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 
 name = "Enrique Valencia"
 age = 19
-favortie_food = "crabs" 
+favorite_food = "crabs" 
 height_in_inches = 73
 height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? "You are taller than average" : "You are not taller than average"
 ## Do not modify below this line!
